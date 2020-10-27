4ce83c945e718bf0a056b467858a2f2f62b65e88
Redven-Biker <marween35620@gmail.com>
2020-10-27 01:42:21 +0100

    1.0

---
+++ __resource.lua
@@ -0,0 +1,12 @@
+files {
+    'index.html',
+    'css/style.css',
+    'img/background.png',
+    'img/logo.png'
+}
+
+loadscreen 'index.html'
+loadscreen_manual_shutdown 'yes'
+client_script 'client.lua'
+
+resource_manifest_version '77731fab-63ca-442c-a67b-abc70f28dfa5'
